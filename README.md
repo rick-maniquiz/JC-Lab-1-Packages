e80ea8eb89443090c5a82f99f10e523c064c6907
rick-maniquiz <rafaelnico.maniquiz@gcash.com>
2025-07-31 13:55:03 +0800

    Final Submission, Laboratory: Java Packages

---
+++ README.md
@@ -0,0 +1,8 @@
+# [FECP5-1009] Laboratory: Java Packages
+
+### Cadet Name: Rafael Nico T. Maniquiz
+### Output and Project Structure:
+<img src="https://github.com/rick-maniquiz/JC-Lab-1-Packages/blob/09a96ab62f009a2c714b56f11a72257d8c338af5/screenshots/Screenshot%202025-07-31%20at%201.47.57%E2%80%AFPM.png"/>
+
+### Test Results:
+<img src="https://github.com/rick-maniquiz/JC-Lab-1-Packages/blob/09a96ab62f009a2c714b56f11a72257d8c338af5/screenshots/Screenshot%202025-07-31%20at%201.48.34%E2%80%AFPM.png"/>
--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,18 +1,19 @@
-package org.example;
+package org.example.main;
 
 
 // Import here manually
 import org.example.packageone.ClassOne;
 import org.example.packagetwo.ClassTwo;
 import org.example.packagethree.ClassThree;
+import static java.lang.System.out;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println((new ClassOne()).greet());
-        System.out.println((new ClassTwo()).greet());
-        System.out.println((new ClassThree()).greet());
+        out.println((new ClassOne()).greet());
+        out.println((new ClassTwo()).greet());
+        out.println((new ClassThree()).greet());
 
     }
 }
